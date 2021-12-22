b8f41be16b20e86258221553ab97599ecca68d4e
acevedo-adrian <acevedohugoadrian@gmail.com>
2021-12-21 21:26:57 -0300

    Insert items into firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Componentes propios
-import NavBar from './components/NavBar/NavBar';
-import Home from './components/Home/Home';
-import ItemsListsContiner from './components/ItemListCotainer/ItemsListsContiner';
-import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
-import Cart from './components/Cart/Cart';
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./components/Home/Home";
+import ItemsListsContiner from "./components/ItemListCotainer/ItemsListsContiner";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import Cart from "./components/Cart/Cart";
 
-import { CartProvider } from './context/CartContext';
+import { CartProvider } from "./context/CartContext";
 const App = () => {
   return (
     <>
@@ -15,16 +15,22 @@ const App = () => {
         <BrowserRouter>
           <NavBar />
           <Routes>
-            <Route path="/" element={<Home title='DRINK COFFEE' />} />
-            <Route path="/products" element={<ItemsListsContiner title="PRODUCTOS" />} />
-            <Route path="/category/:category" element={<ItemsListsContiner title="CAFEE ESTILO" />} />
+            <Route path="/" element={<Home title="DRINK COFFEE" />} />
+            <Route
+              path="/products"
+              element={<ItemsListsContiner title="PRODUCTOS" />}
+            />
+            <Route
+              path="/category/:category"
+              element={<ItemsListsContiner title="CAFEE ESTILO" />}
+            />
             <Route path="/item/:id" element={<ItemDetailContainer />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
         </BrowserRouter>
       </CartProvider>
     </>
-  )
-}
+  );
+};
 
 export default App;
